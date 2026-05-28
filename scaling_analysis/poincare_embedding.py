41c9bca41d7711dd83b6ef567501447e181acdb9
gestalt <gestalt@example.com>
2026-05-28 16:54:43 -0300

    Configure script working directories dynamically to resolve relative paths

--- scaling_analysis/poincare_embedding.py
+++ scaling_analysis/poincare_embedding.py
@@ -1,4 +1,4 @@
-import sys, os; sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import sys, os; root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))); sys.path.append(root_dir); os.chdir(root_dir)
 import os
 import math
 import torch
